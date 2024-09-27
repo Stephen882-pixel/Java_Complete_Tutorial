3bf6950f342f8a7f4c08d20ea63865dc2214ed1a
Stephen881 <stephenomondi005@outlook.com>
2024-09-27 15:15:07 +0300

    Introduction to Java Programming

---
+++ main.java
@@ -0,0 +1,5 @@
+class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world"); // Everything you want to print should be enclosed in a paranthesis and the text to be printed should also be included inside double quotes and each System.out.Println should end with a semicolon at the end 
+    }
+}
